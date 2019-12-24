dd6a0d61aa63e99a324720fceb1a1be500df7186
khopcus <khopcus@gmail.com>
2019-12-24 14:09:58 -0600

    changed to arm only build

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 set -ex
+
 # SET THE FOLLOWING VARIABLES
 # docker hub username
 USERNAME=khopcuskmf
 # image name
 IMAGE=weewx-dockerweb
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $USERNAME/$IMAGE:latest .
+docker buildx build --platform linux/arm64,linux/arm/v7 -t $USERNAME/$IMAGE:latest .
--- release.sh
+++ release.sh
@@ -3,7 +3,6 @@ set -ex
 # SET THE FOLLOWING VARIABLES
 # docker hub username
 USERNAME=khopcuskmf
-
 # image name
 IMAGE=wewwx-dockerweb
 
@@ -24,6 +23,7 @@ git commit -m "version $version"
 git tag -a "$version" -m "version $version"
 git push
 git push --tags
+
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
 
 # push it
